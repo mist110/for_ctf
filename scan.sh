e69518a6ffc9170e0db62d616df8abf07efe0b32
mist <47656640+mist110@users.noreply.github.com>
2025-09-14 16:03:37 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -6,5 +6,8 @@ for port in {1..65535}; do
     timeout 0.1 bash -c "echo >/dev/tcp/$TARGET/$port" 2>/dev/null
     if [ $? -eq 0 ]; then
         echo "Port $port: OPEN"
+        break
+    else
+        echo "Port $port: CLOSED"
     fi
 done
