3281305b8d42d37f57a0a666050cb97d0cacaace
mist <47656640+mist110@users.noreply.github.com>
2025-09-14 14:20:47 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,7 +2,7 @@
 
 TARGET="api"
 
-for port in {30000..65535}; do
+for port in {1..65535}; do
     timeout 0.1 bash -c "echo >/dev/tcp/$TARGET/$port" 2>/dev/null
     if [ $? -eq 0 ]; then
         echo "Port $port: OPEN"
